bb9588c066d8e356bfa4acc7a11f0fe27fb5affb
forpkgh <aba636838@gmail.com>
2026-01-24 10:28:46 +0300

    refactor: added binding, used constraintLayout, used sp,dp, strings moved to strings.xml

--- app/src/main/java/com/example/firstapp_Ababkova/MainActivity.kt
+++ app/src/main/java/com/example/firstapp_Ababkova/MainActivity.kt
@@ -7,23 +7,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.firstapp_Ababkova.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        setContentView(R.layout.activity_main)
-        val button: Button = findViewById(R.id.button)
-        val text: TextView = findViewById(R.id.text)
-        button.setOnClickListener{
+        binding.button.setOnClickListener{
             view ->
-            text.text = (1..6).random().toString()
+            binding.text.text = (1..6).random().toString()
         }
     }
 }
